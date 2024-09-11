7c9e9e107693e2db1ffbcef5bf392392ddda4ce8
delcroip <patrick.delcroix@swisstph.ch>
2024-09-11 10:59:10 +0200

    update for relase 24.10

--- python/gh-make-release-openimis-json.py
+++ python/gh-make-release-openimis-json.py
@@ -1,5 +1,5 @@
 from config import  RELEASE_NAME
-from utils import get_repos_name,for_repos
+from utils import get_repos_name,for_repos, get_config
 import re
 import json
 import semantic_version # pip install semantic-version
@@ -7,7 +7,7 @@ import semantic_version # pip install semantic-version
 def main():
     release_name = RELEASE_NAME
     repos_name = get_repos_name(ref_branch='develop')
-    config = for_repos(repos_name,release_name, '', callback)
+    config = for_repos(repos_name,release_name, release_name, get_config)
     print_fe_config(list(filter(lambda c: c['scope'] == 'fe', config)))
     print_be_config(list(filter(lambda c: c['scope'] == 'be', config)))
         
@@ -15,52 +15,27 @@ def main():
 def print_be_config(modules):
     print('be,{1},https://github.com/{0}/releases/tag/{3}, ,{2}'.format('openimis/openimis-be','assembly',RELEASE_NAME,RELEASE_NAME ))
     for module in modules:
-        print('be,{1},https://github.com/{0}/releases/tag/{2}, https://pypi.org/project/openimis-be-{1}/{3}/,{2}'.format(module['git'],module['name'],module['version'],module['clean_version'],module['nickname'] ))
+        print('be,{1},https://github.com/{0}/releases/tag/{2}, https://pypi.org/project/openimis-be-{1}/{3}/,{2}'.format(module['url'],module['name'],module['version'],module['clean_version'],module['nickname'] ))
     print("BE config")
     for module in modules:
-        print("""            {{
-            "name": "{}",
-            "pip": "{}=={}"
-        }},""".format(module['nickname'],module['name'], module['version'] ))
+        print(f"""            {{
+            "name": "{module['nickname']}",
+            "npm": "{module['url']}@{RELEASE_NAME}#egg={module['name']}"
+        }},""")
+    
+   
     
 def print_fe_config(modules):   
     print('fe,{1},https://github.com/{0}/releases/tag/{2}, ,{2}'.format('openimis/openimis-fe','assembly',RELEASE_NAME,RELEASE_NAME ))
     for module in modules:
-        print('fe,{1},https://github.com/{0}/releases/tag/{2}, https://www.npmjs.com/package/@openimis/fe-{1}/v/{3}/,{2}'.format(module['git'],module['name'],module['version'],module['clean_version'],module['nickname'] ))
+        print('fe,{1},https://github.com/{0}/releases/tag/{2}, https://www.npmjs.com/package/@openimis/fe-{1}/v/{3}/,{2}'.format(module['url'],module['name'],module['version'],module['clean_version'],module['nickname'] ))
 
-
-    
     print("FE config")
     for module in modules:
-        print("""            {{
-            "name": "{}",
-            "npm": "{}@>={}"
-        }},""".format(module['nickname'],module['name'], module['version']))
-
-
-def callback(repo,branches,release_name, to_branch):
-    print(repo.name)
-    tag = repo.get_latest_release().tag_name
-    if 'openimis-fe' in repo.name:
-        config = {'scope':'fe', 'git':repo.organization.login + '/'+ repo.name, 'version': tag, 'clean_version' : tag[1:] if tag.startswith('v') else tag }
-        package_conf = json.loads(repo.get_contents("package.json", ref = 'develop' ).decoded_content)
-        config['name'] = package_conf['name']
-        if config['name'] == '@openimis/fe':
-            config['nickname']= "CoreModule"
-        else:
-            package_conf = repo.get_contents("src/index.js", ref ='develop').decoded_content.decode('utf-8')
-            config['nickname']=re.search(r'export +const +(\w+)Module += +\(cfg\) +=>',package_conf ).group(1)
-            if config['nickname'] is None:
-                config['nickname'] = re.search(r'fe-(.+)$',package_conf['name'] ).group(1).capitalize()+"Module"
-            else:
-                config['nickname'] = config['nickname']+"Module"
-    else:
-        config = {'scope':'be', 'git':repo.organization.login + '/'+ repo.name, 'version': tag, 'clean_version' : tag[1:] if tag.startswith('v') else tag } 
-        package_conf = repo.get_contents("setup.py", ref = 'develop' ).decoded_content.decode('utf-8')
-        config['name']=re.search(r'name *= *[\'|""](.+)[\'|"]',package_conf ).group(1)
-        config['nickname'] =re.search(r'openimis-be-(.+)$',config['name'] ).group(1).replace('-','_')
-    return config
-
+        print(f"""       {{
+            "name": "{module['nickname']}",
+            "npm": "{module['name']}@{module['url']}#{RELEASE_NAME}"
+        }},""")
 
 
     
--- python/utils.py
+++ python/utils.py
@@ -107,3 +107,32 @@ def for_repos(repos_name,from_branch, to_branch, callback):
         if res:
             output.append(res)
     return output
+
+def get_config(repo, branches, source_branch, target_branch):
+    config = {}
+    config['url'] = repo.git_url
+    
+    try:
+        config['version'] = repo.get_latest_release().tag_name
+    except:
+        config['version'] = 'v1.0.0'
+    config ['clean_version'] = config['version'][1:] if config['version'].startswith('v') else config['version']
+    if 'openimis-fe-' in repo.name :
+        config['scope'] = 'fe'
+        package_conf = json.loads(repo.get_contents("package.json", ref = target_branch ).decoded_content)
+        config['name'] = package_conf['name']
+        if config['name'] == '@openimis/fe':
+            config['nickname']= "CoreModule"
+        else:
+            package_conf = repo.get_contents("src/index.js", ref = target_branch).decoded_content.decode('utf-8')
+            config['nickname']=re.search(r'export +const +(\w+)Module += +\(cfg\) +=>',package_conf ).group(1)
+            if config['nickname'] is None:
+                config['nickname'] = re.search(r'fe-(.+)$',package_conf['name'] ).group(1).capitalize()+"Module"
+            else:
+                config['nickname'] = config['nickname']+"Module"
+    else:
+        config['scope'] = 'be'
+        package_conf = repo.get_contents("setup.py", ref = target_branch ).decoded_content.decode('utf-8')
+        config['name']=re.search(r'name *= *[\'|""](.+)[\'|"]',package_conf ).group(1)
+        config['nickname'] =re.search(r'openimis-be-(.+)$',config['name'] ).group(1).replace('-','_')
+    return config
